d66bfa159ae38ca31e9102085c1d4b3e1659f0c1
Justin J. Janes <VestigeJ@users.noreply.github.com>
2022-09-21 15:53:17 -0700

    yeah porque

---
+++ run-win-agent.ps1
@@ -0,0 +1,23 @@
+$SERVER = Read-Host -Prompt 'input the server ip to connect this windows agent to: '
+$TOKEN = Read-Host -Prompt 'paste in the token from the server nodes: '
+$VERSION = Read-Host -Prompt 'paste the version to test: '
+Write-Host "Setting up Windows agent, talking to...  '$SERVER"
+
+Enable-WindowsOptionalFeature -Online -FeatureName Containers –All -Wait
+New-Item -Type Directory c:/etc/rancher/rke2 -Force -Wait
+Set-Content -Path c:/etc/rancher/rke2/config.yaml -Value @"
+server: https://$SERVER:9345
+token: $TOKEN
+kube-proxy-arg: feature-gates=IPv6DualStack=false
+"@ -Wait
+[Environment]::SetEnvironmentVariable(
+    "Path",
+    [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";c:\var\lib\rancher\rke2\bin;c:\usr\local\bin;c:\var\lib\rancher\rke2\",
+    [EnvironmentVariableTarget]::Machine)
+
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/rancher/rke2/master/install.ps1 -Outfile [System.Environment]::CurrentDirectory\install.ps1 -Wait
+#Invoke-RestMethod -Uri https://raw.githubusercontent.com/rancher/rke2/master/install.ps1 Add-Content [System.Environment]::CurrentDirectory\run-win-agent.ps1
+
+$PSScriptRoot\install.ps1 -Version $VERSION -Wait
+$PSScriptRoot\rke2.exe agent service --add -Wait
+Start-Service rke2
