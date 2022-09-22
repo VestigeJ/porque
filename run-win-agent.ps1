47967569eceb6d09fee6302a68d65913568b91cb
Justin J. Janes <VestigeJ@users.noreply.github.com>
2022-09-21 17:00:40 -0700

    grumble

--- run-win-agent.ps1
+++ run-win-agent.ps1
@@ -1,7 +1,7 @@
 $SERVER = Read-Host -Prompt 'input the server ip to connect this windows agent to: '
 $TOKEN = Read-Host -Prompt 'paste in the token from the server nodes: '
 $VERSION = Read-Host -Prompt 'paste the version to test: '
-Write-Host "Setting up Windows agent, talking to...  '$SERVER"
+Write-Host "Setting up Windows agent, talking to the server at... '$SERVER"
 
 Enable-WindowsOptionalFeature -Online -FeatureName Containers -All
 New-Item -Type Directory c:/etc/rancher/rke2 -Force
@@ -10,8 +10,7 @@ server: https://$SERVER:9345
 token: $TOKEN
 kube-proxy-arg: feature-gates=IPv6DualStack=false
 '@
-Invoke-RestMethod -Uri https://raw.githubusercontent.com/rancher/rke2/master/install.ps1 -Outfile [System.Environment]::CurrentDirectory\install.ps1
-#Invoke-RestMethod -Uri https://raw.githubusercontent.com/rancher/rke2/master/install.ps1 Add-Content [System.Environment]::CurrentDirectory\run-win-agent.ps1
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/rancher/rke2/master/install.ps1 -Outfile [System.Environment]::CurrentDirectory\install.ps1
 & 'install.ps1' -Version $VERSION
 & 'rke2.exe' agent service --add
 Start-Service rke2
